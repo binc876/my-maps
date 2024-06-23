4eb0cc57662a834d3f6245cc10b3ec9ba9b3f7fc
pazcalll <yazeedarifin@gmail.com>
2024-06-24 01:48:02 +0700

    feat: reset password

--- src/pages/forgotpassword/VerifPassword.jsx
+++ src/pages/forgotpassword/VerifPassword.jsx
@@ -1,38 +1,67 @@
 import { Button, Col, Container, Form, Row } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
 
 import heroUbahPassword from '../../assets/heroUbahPassword.svg'
+import { useState } from 'react'
+
+import axios from 'axios'
+import { env } from '../../../config'
+
 
 export default function VerifPassword() {
-  const navigate = useNavigate()
+    const resetToken = new URLSearchParams(window.location.search).get('token')
+    const resetEmail = new URLSearchParams(window.location.search).get('email')
 
-  return (
-    <div className='change-password'>
-        <header className='w-100 min-vh-100 d-flex align-items-center'>
-            <Container>
-                <Row className='header-box d-flex align-items-center'>
-                    <Col lg='6' className='change-pass-card'>
-                        <p>Change your password with a new one</p>
+    const [token, setToken] = useState(resetToken)
+    const [email, setEmail] = useState(resetEmail)
+    const [password, setPassword] = useState(null)
+    const [passwordConfirmation, setPasswordConfirmation] = useState(null)
 
-                        <Form className='mt-4 mb-4'>
-                            <Form.Group>
-                                <Form.Control type='password' placeholder='New password' className='mb-3'/>
-                                <Form.Control type='password' placeholder='Confirm new password'/>
-                            </Form.Group>
-                            <Form.Text muted>
-                                Your password must be 8-20 characters long, contain letters and numbers,
-                                and must not contain spaces, special characters, or emoji.
-                            </Form.Text>
-                        </Form>
+    const handleSubmission = (event) => {
+        event.preventDefault()
 
-                        <Button variant='warning' onClick={() => navigate('/')}>Change Password</Button>
-                    </Col>
-                    <Col lg='6' className='pt-lg-0 pt-5'>
-                        <img className='hero' src={heroUbahPassword}/>
-                    </Col>
-                </Row>
-            </Container>
-        </header>
-    </div>
-  )
+        axios({
+            method: 'post',
+            url: `${env.BACKEND_URL}/api/user/reset-password`,
+            data: {
+                email: email,
+                password: password,
+                password_confirmation: passwordConfirmation,
+                token: token
+            }
+        }).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    return (
+        <div className='change-password'>
+            <header className='w-100 min-vh-100 d-flex align-items-center'>
+                <Container>
+                    <Row className='header-box d-flex align-items-center'>
+                        <Col lg='6' className='change-pass-card'>
+                            <p>Change your password with a new one</p>
+
+                            <Form className='mt-4 mb-4' onSubmit={handleSubmission}>
+                                <Form.Group>
+                                    <Form.Control type='password' placeholder='New password' className='mb-3' onInput={(e) => setPassword(e.target.value)}/>
+                                    <Form.Control type='password' placeholder='Confirm new password' onInput={(e) => setPasswordConfirmation(e.target.value)}/>
+                                </Form.Group>
+                                {/* <Form.Text muted>
+                                    Your password must be 8-20 characters long, contain letters and numbers,
+                                    and must not contain spaces, special characters, or emoji.
+                                </Form.Text> */}
+                            <Button variant='warning' className='mt-3' type='submit'>Change Password</Button>
+
+                            </Form>
+                        </Col>
+                        <Col lg='6' className='pt-lg-0 pt-5'>
+                            <img className='hero' src={heroUbahPassword}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
+        </div>
+    )
 }
