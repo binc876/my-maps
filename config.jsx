a9baf244b9d77346647df8f50698d91c44467f9c
pazcalll <yazeedarifin@gmail.com>
2024-07-11 00:09:47 +0700

    etc: env update

--- config.jsx
+++ config.jsx
@@ -1,4 +1,4 @@
 export const env = {
-    BACKEND_URL: 'http://anesthesiology-brawijaya.cloud:3000',
+    BACKEND_URL: 'http://127.0.0.1:3000',
     BASE_URL: 'https://anesthesiology-brawijaya.cloud'
 }
